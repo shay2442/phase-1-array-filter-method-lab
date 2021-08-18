bed0975ef138f1bdd805e88f030ba18fc76b85e5
Shay Wacker <shay2442@yahoo.com>
2021-08-18 18:07:30 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(collection, possibleMatch) {
+   return collection.filter(name => name.toLowerCase() === possibleMatch.toLowerCase())
+    
+}
+
+//findMatching(drivers, 'Ryan');
+
+function fuzzyMatch(collection, providedLetters) {
+    console.log('collection', collection)
+    console.log('letters', providedLetters)
+    return collection.filter(function(person){
+    return person.startsWith(providedLetters)
+
+    }) 
+    // for(let i=0; i< drivers.length; i++) {
+    //     drivers[i].startsWith('letters') {
+    //         return 
+    //     }
+        
+    // }
+}
+
+let startsWithLetters = drivers.filter(firstLetter())
+
+function matchName(drivers, searchName) {
+    return drivers.filter(driver => driver.name === searchName)
+}
